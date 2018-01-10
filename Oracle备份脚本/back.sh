6369cb3cf64d91fa90441e1d6e6b11edfc178091
Dpoint2086 <416666109@qq.com>
2018-01-10 09:09:15 +0800

    1
    1

--- Oracle备份脚本/back.sh
+++ Oracle备份脚本/back.sh
@@ -1,25 +1,23 @@
 !/bin/sh
-source /home/oracle/.bash_profile
+#以下代码为备份相关的变量设置
+source /home/oracle/.bash_profile #载入Oracle用户的环境变量
 dateTime=`date +%Y_%m_%d` #当前系统时间
 days=7 #删除7天前的备份数据s
 orsid='127.0.0.1:1521/orcl' #oralce连接信息
-orowner=sys # 备份此用户下面的数据
-bakuser=sys #用此用户来执行备份，必须要有备份操作的权限
+orowner=sys # 备份此Oracle用户下面的数据
+bakuser=sys #用此用户来执行备份，注意必须要有备份操作的权限
 bakpass=oracle #执行备注的用户密码
-bakdir=/DATA/bakorcldata/moodle #备份文件路径，需要提前创建好
+bakdir=/DATA/bakorcldata/moodle #备份文件路径，需要提前创建好，并赋权
 USERID="/as sysdba"
 bakdata=$orowner"_"$dateTime.dmp #备份数据库保存的名称
 baklog=$orowner"_"$dateTime.log #备份执行时候生成的日志文件名称
 ordatabak=$orowner"_"$dateTime.tar.gz #最后保存的Oracle数据库备份文件
-#以下代码为要放置备份文件的远程机的信息
-#远程服务器地址
-remoteip=10.121.2.29
-#上传到远程服务器的路径
-remotePath=/opt
-#远程服务器用户名
-remoteuser=root
-#远程服务器密码
-remotepass=123456
+#以下代码为要放置备份文件的远程机的变量信息
+remoteip=10.121.2.29 #远程服务器地址
+remotePath=/opt #上传到远程服务器的路径
+remoteuser=root #远程服务器用户名
+remotepass=123456 #远程服务器密码
+#以下代码为备份执行脚本
 cd $bakdir #进入备份目录
 mkdir -p $orowner #按需要备份的Oracle用户创建目录
 cd $orowner #进入目录
@@ -27,7 +25,7 @@ find $bakdir/$orowner -type f -name "*.log" -exec rm {} \; #删除日志文件
 find $bakdir/$orowner -type f -name "*.dmp" -exec rm {} \; #删除备份文件
 #exp $bakuser/$bakpass@$orsid grants=y owner=$orowner file=$bakdir/$orowner/$bakdata log=$bakdir/$orowner/$baklog #执行备份
 #exp /'as sysdba/' grants=y owner=$orowner file=$bakdir/$orowner/$bakdata log=$bakdir/$orowner/$baklog
-expdp \"'sys/oracle as sysdba'\" grants=y owner=$orowner file=$bakdir/$orowner/$bakdata log=$bakdir/$orowner/$baklog
+expdp \"'sys/oracle as sysdba'\" grants=y owner=$orowner file=$bakdir/$orowner/$bakdata log=$bakdir/$orowner/$baklog #Oracle备份命令
 tar -zcvf $ordatabak $bakdata $baklog #压缩备份文件和日志文件
 find $bakdir/$orowner -type f -name "*.log" -exec rm {} \; #删除日志文件
 find $bakdir/$orowner -type f -name "*.dmp" -exec rm {} \; #删除备份文件
--- Oracle备份脚本/附件备份.sh
+++ Oracle备份脚本/附件备份.sh
@@ -1,23 +1,18 @@
 #!/bin/sh
+#以下为备份所需的变量信息设置
 dateTime=`date +%Y_%m_%d` #当前系统时间
 gzdays=7 #删除7天前的备份数据
 snapdays=7 #删除7天前的快照数据
-sdir=/app1/tomcat7_2.1/webapps/ROOT #
+sdir=/app1/tomcat7_2.1/webapps/ROOT #要备份的源目录路径
 bakdir=/localbackdir #本地备份文件路径，需要提前创建好
+remoteip=10.121.4.1 #远程服务器地址
+remotePath=/opt/Backupdir #上传到远程服务器的路径
+remoteuser=root #远程服务器用户名
+remotepass=123456 #远程服务器密码
 
-#远程服务器地址
-remoteip=10.121.4.1
-#上传到远程服务器的路径
-remotePath=/opt/Backupdir
-#远程服务器用户名
-remoteuser=root
-#远程服务器密码
-remotepass=123456
-
-
+#以下代码为执行脚本
 cd $bakdir #进入备份源目录目录
-tar -g /$bakdir/snapshot_data.snap -zcpf /$bakdir/$dateTime.tar.gz --exclude-from=/$bakdir/exclude.list /$sdir
-
+tar -g /$bakdir/snapshot_data.snap -zcpf /$bakdir/$dateTime.tar.gz --exclude-from=/$bakdir/exclude.list /$sdir #备份命令,exclude.list文档中放置备份所排除的目录路径
 find $bakdir -type f -name "*.tar.gz" -mtime +$gzdays -exec rm -rf {} \; #删除7天前的备份（注意：{} \中间有空格）
 find $bakdir -type f -name "*.snap" -mtime +$snapdays -exec rm -rf {} \; #删除7天前的快照数据（注意：{} \中间有空格）
 #scp $bakdir/$dateTime.tar.gz $remoteuser@$remoteip:$remotePath #将备份文件上传到远程服务器
